c5d1a0764bf6b1b34ae4f3b7347924209bcad7e7
Calvin Roberts <roberts.calvin85@gmail.com>
2023-10-25 23:38:49 +0100

    Create mousemove function

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 'use strict';
+let cards = document.querySelectorAll('.card');
+cards.forEach((card) => {
+  card.onmousemove = function (e) {
+    let x = e.pageX - card.offsetLeft;
+    let y = e.pageY - card.offsetTop;
+
+    card.style.setProperty('--x', x + 'px');
+    card.style.setProperty('--y', y + 'px');
+  };
+});
